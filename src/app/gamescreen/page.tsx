23a530480b215d4ebebf0ed9a3e584a0413b695b
Jesus Guzman <amorales130@alumnos.uaq.mx>
2024-10-21 15:58:46 -0600

    Fix: Fixed some front errors

--- src/app/gamescreen/page.tsx
+++ src/app/gamescreen/page.tsx
@@ -3,7 +3,6 @@ import CardExercise from "@/components/CardExercise";
 import RolltheDice from "@/components/RolltheDice";
 import React from "react";
 
-
 interface PageProps {
   searchParams: { turnoJugador?: string };
 }
@@ -12,16 +11,21 @@ export default function Page({ searchParams }: PageProps) {
   const turnoJugador = parseInt(searchParams.turnoJugador || "1", 10);
 
   return (
-    <div className="flex flex-col w-full h-full gap-16">
+    <div className="flex flex-col w-full min-h-screen gap-16 sm:gap-8 px-4">
+      {/* Contenedor de RolltheDice */}
       <div className="flex w-full h-full items-center justify-center">
         <RolltheDice turnoJugador={turnoJugador} />
       </div>
+
+      {/* Contenedor de CardExercise */}
       <div className="flex w-full h-full justify-center items-center">
-        <CardExercise/>
+        <CardExercise />
       </div>
-      <footer className="flex w-full h-full justify-end">
-        <ButtonModal/>
+
+      {/* Footer responsivo */}
+      <footer className="flex w-full justify-end sm:justify-center items-center mt-4 sm:mt-2">
+        <ButtonModal />
       </footer>
     </div>
-  )
+  );
 }
--- src/app/rolldice/page.tsx
+++ src/app/rolldice/page.tsx
@@ -1,4 +1,3 @@
-// app/rolldice/page.tsx
 import React from "react";
 import RolltheDice from "@/components/RolltheDice";
 import Dice from "@/components/Dice";
@@ -11,15 +10,22 @@ export default function Page({ searchParams }: PageProps) {
   const turnoJugador = parseInt(searchParams.turnoJugador || "1", 10);
 
   return (
-    <div className="flex flex-col w-full h-full gap-16">
+    <div className="flex flex-col w-full min-h-screen gap-16 sm:gap-8 items-center">
+      {/* Contenedor de RolltheDice */}
       <div className="flex w-full h-full items-center justify-center">
         <RolltheDice turnoJugador={turnoJugador} />
       </div>
+
+      {/* Texto "Lanza el dado" */}
       <div className="w-full h-full">
-        <h1 className="text-2xl text-center text-text-900">Lanza el dado</h1>
+        <h1 className="text-2xl sm:text-xl text-center text-text-900">
+          Lanza el dado
+        </h1>
       </div>
-      <div>
-        <Dice turnoJugador={turnoJugador}/>
+
+      {/* Contenedor del componente Dice */}
+      <div className="w-full flex justify-center">
+        <Dice turnoJugador={turnoJugador} />
       </div>
     </div>
   );
